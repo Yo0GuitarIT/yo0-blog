6a4b81109f889eaef32c16f68aeb751ede5f05c8
Yu Ling Chen <yo0.guitar.it@gmail.com>
2026-06-27 02:20:46 +0800

    feat: add theme toggle functionality and improve layout
    - Implemented a theme toggle button in the Header component to switch between light and dark modes.
    - Added script to persist theme preference in localStorage and apply it on page load in the Layout component.
    - Updated global CSS to support dark mode styling.

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig, fontProviders } from "astro/config";
-
+import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
